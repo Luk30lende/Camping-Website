da25431b5ba0087811605eaf82a6645323fa0121
Luke Olende <olendeluke2k@gmail.com>
2025-01-04 21:29:31 +0300

    Add media queries and gsap animations

--- app.js
+++ app.js
@@ -27,5 +27,19 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from(".home_img-2", 1.2, { opacity: 0, y: 200, delay: 0.1 });
+gsap.from(".home_img-3", 1.2, { opacity: 0, y: 200, delay: 0.5 });
+gsap.from(".home_data", 1.2, { opacity: 0, y: -60, delay: 1 });
+gsap.from(".home_bird-1", 1.2, { opacity: 0, y: -80, delay: 1.1 });
+gsap.from(".home_bird-2", 1.2, { opacity: 0, y: 80, delay: 1.2 });
+gsap.from(".home_img-1", 1.2, { opacity: 0, y: 200, delay: 1.2 });
+gsap.from(".home_img-4", 1.2, { opacity: 0, y: 200, delay: 1.3 });
